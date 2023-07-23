0a6d5d5424c5e7ba168fedb2ec26a27de4f5a993
mawosoft <78562192+mawosoft@users.noreply.github.com>
2023-07-23 04:43:13 +0200

    Enable analyzers. - Enable all latest code analyzers. - Add .globalconfig for src project. - Fix warnings in src project.

--- src/Mawosoft.PowerShell.WindowsSearchManager/SearchCatalogCommands.cs
+++ src/Mawosoft.PowerShell.WindowsSearchManager/SearchCatalogCommands.cs
@@ -13,6 +13,7 @@ public sealed class GetSearchCatalogCommand : SearchApiCommandBase
     [ValidateNotNullOrEmpty()]
     public string? Catalog { get; set; }
 
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Continue after WriteError.")]
     protected override void EndProcessing()
     {
         ISearchManager manager = CreateSearchManager();
@@ -161,8 +162,9 @@ public sealed class UpdateSearchCatalogCommand : SearchApiCommandBase
     public string Catalog { get; set; } = DefaultCatalogName;
 
     private ISearchCatalogManager? _catalogManager;
-    private ISearchCatalogManager CatalogManager { get => _catalogManager ??= GetCatalogManager(Catalog); }
+    private ISearchCatalogManager CatalogManager => _catalogManager ??= GetCatalogManager(Catalog);
 
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Continue after WriteError.")]
     protected override void ProcessRecord()
     {
         if (ParameterSetName == AllParameterSet)
