7b2150384a029b80351463a9cc77e712549ea8e1
mawosoft <78562192+mawosoft@users.noreply.github.com>
2023-08-29 19:14:56 +0200

    Add RemoveSearchRoot/Rule/ResetSearchRuleCommandTests.

---
+++ tests/Mawosoft.PowerShell.WindowsSearchManager.Tests/ResetSearchRuleCommandTests.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+namespace Mawosoft.PowerShell.WindowsSearchManager.Tests;
+
+public class ResetSearchRuleCommandTests : CommandTestBase
+{
+    [Fact]
+    public void Command_Succeeds()
+    {
+        Collection<PSObject> results = InvokeScript("Reset-SearchRule ");
+        Assert.Empty(results);
+        Assert.False(PowerShell.HadErrors);
+        Assert.Equal("RevertToDefaultScopes()", Assert.Single(InterfaceChain.ScopeManager.RecordedCalls));
+        Assert.True(InterfaceChain.SingleHasWriteRecordings(InterfaceChain.ScopeManager));
+    }
+
+    [Theory]
+    [ClassData(typeof(Exception_TheoryData))]
+    public void Command_HandlesFailures(ExceptionParam exceptionParam)
+    {
+        InterfaceChain.ScopeManager.AddException("^RevertToDefaultScopes$", exceptionParam.Exception);
+        Collection<PSObject> results = InvokeScript("Reset-SearchRule ");
+        Assert.Empty(results);
+        AssertSingleErrorRecord(exceptionParam);
+    }
+}
