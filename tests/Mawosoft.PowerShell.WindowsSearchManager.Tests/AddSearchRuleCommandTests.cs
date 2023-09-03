826da06ddf2c4c127bd010532720ca3d1bf211ed
mawosoft <78562192+mawosoft@users.noreply.github.com>
2023-09-03 20:31:12 +0200

    Add SearchRule tests (Add, Get, Remove, Reset, Test).

---
+++ tests/Mawosoft.PowerShell.WindowsSearchManager.Tests/AddSearchRuleCommandTests.cs
@@ -0,0 +1,186 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+namespace Mawosoft.PowerShell.WindowsSearchManager.Tests;
+
+public class AddSearchRuleCommandTests : CommandTestBase
+{
+    private Collection<PSObject> InvokeInputParameterSet(List<SearchRuleInfo> ruleInfos, string valueType, bool usePipeline)
+    {
+        IList<object> inputValues;
+        switch (valueType)
+        {
+            case nameof(SearchRuleInfo):
+                inputValues = ruleInfos.ConvertAll(r => r.Clone());
+                break;
+            case nameof(PSObject):
+                inputValues = ruleInfos.ConvertAll(r => (object)new PSObject(r.Clone()));
+                break;
+            case nameof(PSCustomObject):
+                PSObject defaultInfo = new(new SearchRuleInfo());
+                inputValues = new List<object>(ruleInfos.Count);
+                foreach (SearchRuleInfo info in ruleInfos)
+                {
+                    PSObject source = new(info);
+                    PSObject result = new();
+                    foreach (var p in source.Properties)
+                    {
+                        if (!p.Value.Equals(defaultInfo.Properties[p.Name].Value))
+                        {
+                            result.Properties.Add(new PSNoteProperty(p.Name, p.Value));
+                        }
+                    }
+                    inputValues.Add(result);
+                }
+                break;
+            default:
+                throw new ArgumentException(null, nameof(valueType));
+        }
+
+        if (usePipeline)
+        {
+            return InvokeScript("Add-SearchRule", inputValues);
+        }
+        else
+        {
+            object values = inputValues.Count == 1 ? inputValues[0] : inputValues;
+            Dictionary<string, object> parameters = new() { { "InputObject", values } };
+            return InvokeCommand("Add-SearchRule", parameters);
+        }
+    }
+
+    private static List<string> GetExpectedMethodCalls(List<SearchRuleInfo> ruleInfos)
+        => ruleInfos.ConvertAll(r => r.RuleSet == SearchRuleInfo.SearchRuleSet.User
+            ? $"AddUserScopeRule({r.Path},{(r.RuleType == SearchRuleInfo.SearchRuleType.Include ? 1 : 0)},{(r.OverrideChildren ? 1 : 0)},{(uint)r.FollowFlags})"
+            : $"AddDefaultScopeRule({r.Path},{(r.RuleType == SearchRuleInfo.SearchRuleType.Include ? 1 : 0)},{(uint)r.FollowFlags})");
+
+    private void Assert_InvokeSucceeded(Collection<PSObject> results, IList<string> expectedMethodCalls)
+    {
+        Assert.Empty(results);
+        Assert.False(PowerShell.HadErrors);
+        List<string> recordedCalls = InterfaceChain.ScopeManager.RecordedCalls;
+        Assert.All(recordedCalls.Take(recordedCalls.Count - 1),
+            (item, i) => Assert.Equal(expectedMethodCalls[i], item));
+        Assert.Equal(expectedMethodCalls.Count, recordedCalls.Count - 1);
+        Assert.Equal("SaveAll()", InterfaceChain.ScopeManager.RecordedCalls[recordedCalls.Count - 1]);
+        Assert.True(InterfaceChain.SingleHasWriteRecordings(InterfaceChain.ScopeManager));
+    }
+
+    private static readonly List<SearchRuleInfo> s_ruleInfos = new()
+    {
+        new()
+        {
+            Path = @"x:\foo",
+            RuleType = SearchRuleInfo.SearchRuleType.Include,
+            RuleSet = SearchRuleInfo.SearchRuleSet.Default,
+            FollowFlags = SearchRuleInfo._FOLLOW_FLAGS.FF_SUPPRESSINDEXING,
+        },
+        new()
+        {
+            Path = @"x:\foo\foo",
+            RuleType = SearchRuleInfo.SearchRuleType.Exclude,
+            RuleSet = SearchRuleInfo.SearchRuleSet.Default,
+        },
+        new()
+        {
+            Path = @"x:\foo\bar",
+            RuleType = SearchRuleInfo.SearchRuleType.Exclude,
+            RuleSet = SearchRuleInfo.SearchRuleSet.User,
+            OverrideChildren = true
+        },
+        new()
+        {
+            Path = @"x:\bar\foo",
+            RuleType = SearchRuleInfo.SearchRuleType.Include,
+            RuleSet = SearchRuleInfo.SearchRuleSet.User,
+            FollowFlags = SearchRuleInfo._FOLLOW_FLAGS.FF_SUPPRESSINDEXING,
+        },
+        new()
+        {
+            Path = @"x:\foo\baz\bar",
+            RuleType = SearchRuleInfo.SearchRuleType.Include,
+            RuleSet = SearchRuleInfo.SearchRuleSet.User,
+            OverrideChildren = true
+        }
+    };
+
+    [Theory]
+    [InlineData(@"Add-SearchRule -Path x:\foo -RuleType Include", @"AddUserScopeRule(x:\foo,1,0,1)")]
+    [InlineData(@"Add-SearchRule -Path x:\foo -RuleType Exclude -OverrideChildren", @"AddUserScopeRule(x:\foo,0,1,1)")]
+    [InlineData(@"Add-SearchRule -Path x:\foo, x:\bar -RuleType Include -OverrideChildren", @"AddUserScopeRule(x:\foo,1,1,1)", @"AddUserScopeRule(x:\bar,1,1,1)")]
+    [InlineData(@"Add-SearchRule x:\foo, x:\bar Default Include", @"AddDefaultScopeRule(x:\foo,1,1)", @"AddDefaultScopeRule(x:\bar,1,1)")]
+    [InlineData(@"@('x:\foo', 'x:\bar') | Add-SearchRule -RuleSet 1 -RuleType Exclude", @"AddDefaultScopeRule(x:\foo,0,1)", @"AddDefaultScopeRule(x:\bar,0,1)")]
+    public void PathParameterSet_Succeeds(string script, params string[] expectedMethodCalls)
+    {
+        Collection<PSObject> results = InvokeScript(script);
+        Assert_InvokeSucceeded(results, expectedMethodCalls);
+    }
+
+    [Theory]
+    [InlineData(1, nameof(SearchRuleInfo), false)]
+    [InlineData(1, nameof(PSObject), false)]
+    [InlineData(1, nameof(PSCustomObject), false)]
+    [InlineData(-1, nameof(SearchRuleInfo), false)]
+    [InlineData(-1, nameof(PSObject), false)]
+    [InlineData(-1, nameof(PSCustomObject), false)]
+    [InlineData(-1, nameof(SearchRuleInfo), true)]
+    [InlineData(-1, nameof(PSObject), true)]
+    public void InputParameterSet_Succeeds(int valueCount, string valueType, bool usePipeline)
+    {
+        if (valueCount <= 0) valueCount = s_ruleInfos.Count;
+        List<SearchRuleInfo> ruleInfos = s_ruleInfos.GetRange(0, valueCount);
+        Collection<PSObject> results = InvokeInputParameterSet(ruleInfos, valueType, usePipeline);
+        Assert_InvokeSucceeded(results, GetExpectedMethodCalls(ruleInfos));
+    }
+
+    [Theory]
+    [InlineData(@"@('x:\foo', 'x:\bar') | Add-SearchRule -RuleSet Default -RuleType Exclude -OverrideChildren", 2, @"AddDefaultScopeRule(x:\foo,0,1)", @"AddDefaultScopeRule(x:\bar,0,1)")]
+    public void PathParameterSet_RuleSetDefault_WithOverrideChildren_WritesWarnings(string script, int expectedWarningCount, params string[] expectedMethodCalls)
+    {
+        Collection<PSObject> results = InvokeScript(script);
+        Assert_InvokeSucceeded(results, expectedMethodCalls);
+        Assert.Equal(expectedWarningCount, PowerShell.Streams.Warning.Count);
+    }
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void InputParameterSet_RuleSetDefault_WithOverrideChildren_WritesWarnings(bool usePipeline)
+    {
+        List<SearchRuleInfo> ruleInfos = s_ruleInfos.ConvertAll(r =>
+        {
+            SearchRuleInfo r2 = (SearchRuleInfo)r.Clone();
+            r2.RuleSet = SearchRuleInfo.SearchRuleSet.Default;
+            return r2;
+        });
+        int expectedWarningCount = ruleInfos.Count(r => r.OverrideChildren);
+        Assert.InRange(expectedWarningCount, 1, ruleInfos.Count - 1);
+        Collection<PSObject> results = InvokeInputParameterSet(ruleInfos, nameof(PSObject), usePipeline);
+        Assert_InvokeSucceeded(results, GetExpectedMethodCalls(ruleInfos));
+        Assert.Equal(expectedWarningCount, PowerShell.Streams.Warning.Count);
+    }
+
+    public static readonly object?[][] HandlesFailures_TestData =
+        new string[][]
+        {
+            new string[] { @"Add-SearchRule -Path x:\foo -RuleType Include ", "^AddUserScopeRule$" },
+            new string[] { @"Add-SearchRule -Path x:\foo -RuleSet Default -RuleType Include ", "^AddDefaultScopeRule$" },
+            new string[] { @"Add-SearchRule -Path x:\foo -RuleType Include ", "^SaveAll$" },
+            new string[] { @"Add-SearchRule -Path x:\foo -RuleSet Default -RuleType Include ", "^SaveAll$" },
+        }
+        .CrossJoin(new Exception_TheoryData())
+        .ToArray();
+
+    [Theory]
+    [MemberData(nameof(HandlesFailures_TestData))]
+    public void Command_HandlesFailures(string script, string exceptionRegex, ExceptionParam exceptionParam)
+    {
+        InterfaceChain.ScopeManager.AddException(exceptionRegex, exceptionParam.Exception);
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.Empty(results);
+        if (exceptionRegex.IndexOf("SaveAll", StringComparison.Ordinal) < 0)
+        {
+            Assert.DoesNotContain("SaveAll()", InterfaceChain.ScopeManager.RecordedCalls);
+        }
+        AssertSingleErrorRecord(exceptionParam);
+    }
+}
---
+++ tests/Mawosoft.PowerShell.WindowsSearchManager.Tests/RemoveSearchRuleCommandTests.cs
@@ -0,0 +1,50 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+namespace Mawosoft.PowerShell.WindowsSearchManager.Tests;
+
+public class RemoveSearchRuleCommandTests : CommandTestBase
+{
+    [Theory]
+    [InlineData(@"Remove-SearchRule -Path x:\foo", "RemoveScopeRule({0})", @"x:\foo")]
+    [InlineData(@"Remove-SearchRule -Path x:\foo -RuleSet User", "RemoveScopeRule({0})", @"x:\foo")]
+    [InlineData(@"Remove-SearchRule -Path x:\foo, x:\bar -RuleSet Default", "RemoveDefaultScopeRule({0})", @"x:\foo", @"x:\bar")]
+    [InlineData(@"Remove-SearchRule x:\foo, x:\bar Default", "RemoveDefaultScopeRule({0})", @"x:\foo", @"x:\bar")]
+    [InlineData(@"@('x:\foo', 'x:\bar') | Remove-SearchRule", "RemoveScopeRule({0})", @"x:\foo", @"x:\bar")]
+    [InlineData(@"@([pscustomobject]@{ Path = 'x:\foo'}, [pscustomobject]@{ Path = 'x:\bar'}) | Remove-SearchRule -RuleSet Default", "RemoveDefaultScopeRule({0})", @"x:\foo", @"x:\bar")]
+    public void Command_Succeeds(string script, string expectedMethodcall, params string[] expectedMethodParameters)
+    {
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.Empty(results);
+        Assert.False(PowerShell.HadErrors);
+        List<string> recordedCalls = InterfaceChain.ScopeManager.RecordedCalls;
+        Assert.All(recordedCalls.Take(recordedCalls.Count - 1),
+            (item, i) => Assert.Equal(string.Format(expectedMethodcall, expectedMethodParameters[i]), item));
+        Assert.Equal(expectedMethodParameters.Length, recordedCalls.Count - 1);
+        Assert.Equal("SaveAll()", recordedCalls[recordedCalls.Count - 1]);
+        Assert.True(InterfaceChain.SingleHasWriteRecordings(InterfaceChain.ScopeManager));
+    }
+
+    public static readonly object?[][] HandlesFailures_TestData =
+        new string[][]
+        {
+            new string[] { @"Remove-SearchRule -Path x:\foo ", "^RemoveScopeRule$" },
+            new string[] { @"Remove-SearchRule x:\foo Default ", "^RemoveDefaultScopeRule$" },
+            new string[] { @"Remove-SearchRule -Path x:\foo ", "^SaveAll$" }
+        }
+        .CrossJoin(new Exception_TheoryData())
+        .ToArray();
+
+    [Theory]
+    [MemberData(nameof(HandlesFailures_TestData))]
+    public void Command_HandlesFailures(string script, string exceptionRegex, ExceptionParam exceptionParam)
+    {
+        InterfaceChain.ScopeManager.AddException(exceptionRegex, exceptionParam.Exception);
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.Empty(results);
+        if (exceptionRegex.IndexOf("SaveAll", StringComparison.Ordinal) < 0)
+        {
+            Assert.DoesNotContain("SaveAll()", InterfaceChain.ScopeManager.RecordedCalls);
+        }
+        AssertSingleErrorRecord(exceptionParam);
+    }
+}
