6872c7dc4cf82a74e220b501cc7e202966a23ce4
mawosoft <78562192+mawosoft@users.noreply.github.com>
2023-09-03 20:30:40 +0200

    Add SearchRoot tests (Add, Get, Remove).

---
+++ tests/Mawosoft.PowerShell.WindowsSearchManager.Tests/GetSearchRootCommandTests.cs
@@ -0,0 +1,85 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+namespace Mawosoft.PowerShell.WindowsSearchManager.Tests;
+
+public class GetSearchRootCommandTests : CommandTestBase
+{
+    private static readonly List<MockSearchRoot> s_roots = new()
+    {
+        new()
+        {
+            RootURL = @"fooprotocol://{bar-sid}/",
+            ProvidesNotifications = 0,
+            UseNotificationsOnly = 1,
+            EnumerationDepth = 333,
+            HostDepth = 222,
+            AuthenticationType = _AUTH_TYPE.eAUTH_TYPE_BASIC
+        },
+        new()
+        {
+            RootURL = @"file:///c:\",
+            IsHierarchical = 0,
+            UseNotificationsOnly = 1,
+            EnumerationDepth = 333,
+            HostDepth = 222,
+        },
+        new()
+        {
+            RootURL = @"bar://foo/",
+            FollowDirectories = 0,
+            AuthenticationType = _AUTH_TYPE.eAUTH_TYPE_NTLM
+        },
+        new()
+        {
+            RootURL = @"x:\",
+        },
+        new()
+        {
+            RootURL = @"y:\",
+        }
+    };
+
+    [Theory]
+    [InlineData(0, false)]
+    [InlineData(1, false)]
+    [InlineData(1, true)]
+    [InlineData(3, false)]
+    [InlineData(3, true)]
+    public void Command_Succeeds(int rootCount, bool pathOnly)
+    {
+        InterfaceChain.ScopeManager.Roots = new(s_roots.Take(rootCount));
+        string script = $"Get-SearchRoot {(pathOnly ? "-PathOnly" : "")} ";
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.False(PowerShell.HadErrors);
+        Assert.False(InterfaceChain.HasWriteRecordings());
+        if (pathOnly)
+        {
+            var expected = s_roots.Take(rootCount).Select(r => r.RootURL);
+            var actual = results.Select(o => (string)o.BaseObject);
+            Assert.Equal(expected, actual);
+        }
+        else
+        {
+            var expected = s_roots.Take(rootCount);
+            var actual = results.Select(o => (SearchRootInfo)o.BaseObject);
+            Assert.Equal(expected, actual, SearchRootInfoToMockComparer.Instance);
+        }
+    }
+
+    [Theory]
+    [ClassData(typeof(Exception_TheoryData))]
+    public void Command_WithFailures_PartiallySucceeds(ExceptionParam exceptionParam)
+    {
+        InterfaceChain.ScopeManager.Roots = new(s_roots);
+        int expectedCount = 2;
+        Assert.InRange(expectedCount, expectedCount, s_roots.Count - 2);
+        InterfaceChain.ScopeManager.Roots[expectedCount] = exceptionParam.Exception;
+        Collection<PSObject> results = InvokeScript("Get-SearchRoot -PathOnly");
+        Assert.True(PowerShell.HadErrors);
+        Assert.False(InterfaceChain.HasWriteRecordings());
+        var expected = s_roots.Take(expectedCount).Select(r => r.RootURL);
+        var actual = results.Select(o => (string)o.BaseObject);
+        Assert.Equal(expected, actual);
+        AssertSingleErrorRecord(exceptionParam);
+    }
+}
---
+++ tests/Mawosoft.PowerShell.WindowsSearchManager.Tests/RemoveSearchRootCommandTests.cs
@@ -0,0 +1,49 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+namespace Mawosoft.PowerShell.WindowsSearchManager.Tests;
+
+public class RemoveSearchRootCommandTests : CommandTestBase
+{
+    [Theory]
+    [InlineData(@"Remove-SearchRoot -Path x:\foo", @"x:\foo")]
+    [InlineData(@"Remove-SearchRoot -Path x:\foo, x:\bar", @"x:\foo", @"x:\bar")]
+    [InlineData(@"Remove-SearchRoot x:\foo, x:\bar", @"x:\foo", @"x:\bar")]
+    [InlineData(@"@('x:\foo', 'x:\bar') | Remove-SearchRoot", @"x:\foo", @"x:\bar")]
+    [InlineData(@"@([pscustomobject]@{ Path = 'x:\foo'}, [pscustomobject]@{ Path = 'x:\bar'}) | Remove-SearchRoot", @"x:\foo", @"x:\bar")]
+    [InlineData(@"@([Mawosoft.PowerShell.WindowsSearchManager.SearchRootInfo]@{ Path = 'x:\foo'}, [Mawosoft.PowerShell.WindowsSearchManager.SearchRootInfo]@{ Path = 'x:\bar'}) | Remove-SearchRoot", @"x:\foo", @"x:\bar")]
+    public void Command_Succeeds(string script, params string[] expectedMethodParameters)
+    {
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.Empty(results);
+        Assert.False(PowerShell.HadErrors);
+        List<string> recordedCalls = InterfaceChain.ScopeManager.RecordedCalls;
+        Assert.All(recordedCalls.Take(recordedCalls.Count - 1),
+            (item, i) => Assert.Equal($"RemoveRoot({expectedMethodParameters[i]})", item));
+        Assert.Equal(expectedMethodParameters.Length, recordedCalls.Count - 1);
+        Assert.Equal("SaveAll()", recordedCalls[recordedCalls.Count - 1]);
+        Assert.True(InterfaceChain.SingleHasWriteRecordings(InterfaceChain.ScopeManager));
+    }
+
+    public static readonly object?[][] HandlesFailures_TestData =
+        new string[][]
+        {
+            new string[] { @"Remove-SearchRoot -Path x:\foo ", "^RemoveRoot$" },
+            new string[] { @"Remove-SearchRoot -Path x:\foo ", "^SaveAll$" }
+        }
+        .CrossJoin(new Exception_TheoryData())
+        .ToArray();
+
+    [Theory]
+    [MemberData(nameof(HandlesFailures_TestData))]
+    public void Command_HandlesFailures(string script, string exceptionRegex, ExceptionParam exceptionParam)
+    {
+        InterfaceChain.ScopeManager.AddException(exceptionRegex, exceptionParam.Exception);
+        Collection<PSObject> results = InvokeScript(script);
+        Assert.Empty(results);
+        if (exceptionRegex.IndexOf("SaveAll", StringComparison.Ordinal) < 0)
+        {
+            Assert.DoesNotContain("SaveAll()", InterfaceChain.ScopeManager.RecordedCalls);
+        }
+        AssertSingleErrorRecord(exceptionParam);
+    }
+}
