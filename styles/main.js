f21b2efda78aae3011216c565efcf1156a84994f
mawosoft <mawosoft@users.noreply.github.com>
2023-09-24 20:44:57 +0000

    deploy: 92c805af8e5c212ee75585ccfd0b5463a7d5beb5

---
+++ styles/main.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Matthias Wolf, Mawosoft.
+
+// Very simple language definition to highlight the syntax block of a PowerShell cmdlet.
+
+hljs.registerLanguage('psmeta', function (hljs) {
+    const CMDLETS = {
+        scope: 'built_in',
+        begin: /[A-Z]+-[A-Z]+/,
+    };
+    const PARAMETERS = {
+        scope: 'literal',
+        begin: /-[A-Z]+/,
+    };
+    return {
+        case_insensitive: true,
+        disableAutodetect: true,
+        contains: [CMDLETS, PARAMETERS]
+    }
+});
